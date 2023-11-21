b477a3cbe60c032296c3ae8734bada249a7783bc
maryrosedevega <143856348+maryrosedevega@users.noreply.github.com>
2023-11-21 18:53:17 +0800

    Delete Example directory

--- Example/Example/Utility/Constant.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Constant.swift
-//  Example
-//
-//  Created by William.Weng on 2023/9/11.
-//
-
-import Foundation
--- Example/Example/Utility/Extension.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Extension.swift
-//  Example
-//
-//  Created by William.Weng on 2023/9/11.
-//
-
-import Foundation
-
